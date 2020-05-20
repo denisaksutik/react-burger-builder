3768c3374c007e4c35b591683ff0b0217eb9ff3a
Denis Aksiutsik <Denis@MacBook-Pro-2.local>
2020-05-19 20:55:32 -0700

    Updates

---
+++ precache-manifest.b86bdbe954487072a927ec8d539bd6b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eebec98d8f8d22347b0dd158c57f8578",
+    "url": "/react-burger-builder/index.html"
+  },
+  {
+    "revision": "54044a4ffd53ca355e7f",
+    "url": "/react-burger-builder/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "38776e9ef305567e7f9b",
+    "url": "/react-burger-builder/static/js/2.44fd7232.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-burger-builder/static/js/2.44fd7232.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54044a4ffd53ca355e7f",
+    "url": "/react-burger-builder/static/js/main.a2e9c51d.chunk.js"
+  },
+  {
+    "revision": "a9afeba0a271f08a7c72",
+    "url": "/react-burger-builder/static/js/runtime-main.fcbd7ad6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-burger-builder/static/media/logo.5d5d9eef.svg"
+  }
+]);
